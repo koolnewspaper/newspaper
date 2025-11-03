6db57017b5fb97bd9328e7c9308f8ce1bc554623
Human <152773360+aperson31415@users.noreply.github.com>
2025-11-03 21:34:25 +0800

    Added v2 files

--- vuestuff.js
+++ vuestuff.js
@@ -1,7 +1,31 @@
+function fetchJSONData(path) {
+    return fetch(path)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json()
+        })
+        .then(data => {
+            console.log(data)
+            return data
+        })
+        .catch(error => console.error('Failed to fetch data:', error)); 
+}
+
 function app_ref(data) {
     return Vue.createApp({data() {return data}})
 }
 
-function deploy_app(app, cls) {
-    app.mount("."+cls)
+function deploy_app(app, ref) {
+    app.mount(ref)
 }
+
+window.addEventListener('load', function() {
+    const kool_promise = fetchJSONData("https://raw.githubusercontent.com/koolnewspaper/newspaper/refs/heads/main/data.json");
+    kool_promise.then(data => {
+        console.log(data)
+        deploy_app(app_ref(data), "#body")
+        this.document.getElementById("cover_elem").style.display = "none";
+    });
+});
